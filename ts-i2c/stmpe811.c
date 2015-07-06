3bc7d9783f8b26ad46ee661d4e3b64b42168d8df
brabo <brabo@riseup.net>
2015-07-06 19:51:33 +0200

    split-off to ts-i2c, preparation for use as libopencm3 example

--- ts-i2c/stmpe811.c
+++ ts-i2c/stmpe811.c
@@ -1,5 +1,5 @@
 /**
-Copyright (C) 2015 brabo
+Copyright (C) 2015 brabo <brabo.sil@gmail.com>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
